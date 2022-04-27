4bca293ff4228af0803b482f4218eeda63e31e92
dmeekins <drew.a.meekins@gmail.com>
2022-04-27 19:40:30 -0400

    just solved a big error i've been having all day where my page wouldn't load because mongoose couldn't find links to icons in ejs

--- controllers/crossroads.js
+++ controllers/crossroads.js
@@ -1,67 +1,37 @@
 //setup requirements
-const { profile } = require('console')
+// const { profile } = require('console') 
 const express = require('express')
 const router = express.Router()
 const Profile = require('../models/profiles-model')
 
 
-//get all profiles
+//show routes
 router.get('/', (req, res) => {
-    Profile.find({})
-        .then((profile) => {
-            res.render('index', {profile})
-        })
-        .catch(console.error)
+    res.render('index')
 })
 
-//get by id
+//find routes
 router.get('/:id', (req, res) => {
     Profile.findById(req.params.id)
-        .then((profile) => {
-            res.send(req.params.id)
-        })
-        .catch(console.error)
+        .then((profiles) => res.send(profiles))
 })
 
-//create route
-// router.post('/', async (req, res) => {
-//     Profile.create(req.body)
-//         .then((profile) => {
-//             const profile = new Profile({
-//                 username: req.body.username,
-//                 name: req.body.name,
-//                 dob: req.body.dob,
-//                 location: req.body.location,
-//                 aboutMe: req.body.aboutMe,
-//                 profilePicUrl: req.body.profilePicUrl
-//             })
-//     })
-//     try {
-//         const newProfile = await profile.save()
-//         res.json(newProfile)
-//     }
-//     catch (err){
-//         res.send(console.log('error'))
-//     }
-// })
-
-router.post('/', (req, res) => {
+//create routes
+router.post('/create', (req, res) => {
     Profile.create(req.body)
-        .then((profile) => {
-            res.redirect('/profile', profile)
-        })
-        .catch(console.error)
+        .then((profiles) => res.send(profiles))
 })
 
-//update profile info. Using patch instead of update so only modified data gets updated.
-router.patch(':id', (req, res) => {
-    // res.send(req.params.id)
+//update routes
+router.put('/:id', (req, res) => {
+    Profile.findByIdAndUpdate({_id: req.params.id}, req.body)
+    .then((profiles) => res.send(profiles))
 })
 
-//delete route
+//delete routes
 router.delete('/:id', (req, res) => {
-    // res.send(req.params.id)
+    Profile.findByIdAndRemove({_id: req.params.id}, req.body)
+    .then((profiles) => res.send(profiles))
 })
 
-//export file
 module.exports = router
